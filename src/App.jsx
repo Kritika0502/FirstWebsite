6a45139041c60bb16b842113eed06427d6846eac
Satyam Raj <satyamrajarya@gmail.com>
2024-07-25 22:37:45 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 import Services from './Services'
+import Button from './Button'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <Home />
-      <About />
-      <Contact />
-      <Services />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/services" element={<Services/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+<div>
+<button>Helolo Button</button>
+</div>
+<div>
+Here is some text
+</div>
+    </>
+  )
+}
+
+export default Button
